7822b48cc8a3af9bf5de2b2af1b93bf58e7ec55c
ravi-maithrey <63500743+ravi-maithrey@users.noreply.github.com>
2020-04-11 18:36:07 +0530

    the main test file.
    Maybe other test files will be main-er than this

---
+++ test.py
@@ -0,0 +1,12 @@
+count = 0
+for i in range(1, 100):
+    count += 1
+    for j in range(1, 100):
+        count += 1
+        for k in range(1, 100):
+            count += 1
+            if(i + j + k == 100 and 4*i + 2*j + (k/4) == 100):
+                print(i, j, k, count)
+#optimize this further
+#use a count for each operation
+print(count)
