ba2cec589a7cceee4d86eb34f385463329db41de
ravi-maithrey <63500743+ravi-maithrey@users.noreply.github.com>
2020-04-11 23:14:56 +0530

    final
    optimized to the best of my ability

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 count = 0
-for i in range(1, 100):
+for i in range(1, 99):
     count += 1
-    for j in range(1, 100):
+    for j in range(1, 101 - i):
         count += 1
-        for k in range(1, 100):
+        for k in range(1, 101 - (i + j)):
             count += 1
             if(i + j + k == 100 and 4*i + 2*j + (k/4) == 100):
                 print(i, j, k, count)
