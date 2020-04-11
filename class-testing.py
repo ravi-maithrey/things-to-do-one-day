38be5b04686db868a640be2f40a3894358abce40
ravi-maithrey <63500743+ravi-maithrey@users.noreply.github.com>
2020-04-11 20:34:27 +0530

    Update class-testing.py
    overloading is possible but not exactly overlading

--- class-testing.py
+++ class-testing.py
@@ -1,12 +1,13 @@
 class example:
     name = "name"
     age = 18
-    def __init__(self):
-        print("No input given")
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        print("Values added")
+    def __init__(self, name = "name", age = 18):
+        if(namae == "name" or age == 18):
+            print("No input")
+        else:
+            self.name = name
+            self.age = age
+            print("Values added")
     def disp():
         print(name, "/n", age)
 
